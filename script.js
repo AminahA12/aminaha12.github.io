cdcbe53f321e847cddaf06dfe8b67fa963b3b8d1
AminahA12 <aminahasgharali@gmail.com>
2025-07-03 10:36:30 -0500

    punishment for non-cheese lovers

---
+++ script.js
@@ -0,0 +1,8 @@
+const butt = document.querySelector("button");
+
+butt.addEventListener("click", punishment);
+
+function punishment() {
+    alert("GO TO THE CORNER AND THINK ABOUT WH?AT YOU'VE DONE");
+
+}
