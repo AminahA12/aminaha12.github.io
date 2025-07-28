16ce05852909e0673d301c781e261cc4d0b3b206
AminahA12 <aminahasgharali@gmail.com>
2025-07-28 08:11:21 -0500

    setup for monday

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const butt = document.querySelector("button");
 butt.addEventListener("click", punishment);
 
 function punishment() {
-    alert("GO TO THE CORNER AND THINK ABOUT WH?AT YOU'VE DONE");
+    alert("GO TO THE CORNER AND THINK ABOUT WHAT YOU'VE DONE");
 
 }
